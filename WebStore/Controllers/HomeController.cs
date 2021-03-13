7b74958fff00fc793f43deb19b007e6cababe98b
Fenist <kostik06@gmail.com>
2021-03-13 18:56:41 +0300

    First view

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index() => Content("First Controller Action");
+        public IActionResult Index() => View();
 
         public IActionResult SecondAction(string id) => Content($"Action with value id:{id}");
     }
