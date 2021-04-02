c498a1101e2e31fce7cfd3be01686ae0f46c7d8c
Konstantin <kostik06@gmail.com>
2021-04-02 11:59:31 +0300

    Подготовка сущностей для загрузки в БД

--- WebStore.Domain/Entities/Brand.cs
+++ WebStore.Domain/Entities/Brand.cs
@@ -1,10 +1,15 @@
-﻿using WebStore.Domain.Entities.Base;
+﻿using System.Collections.Generic;
+using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Interfaces;
 
 namespace WebStore.Domain.Entities
 {
+    //[Table("Brand")]
     public class Brand : NamedEntity, IOrderedEntity
     {
+        //[Column("BrandOrder"), TypeName="long"]
         public int Order { get; set; }
+
+        public ICollection<Product> Products { get; set; }
     }
 }
