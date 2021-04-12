ced5f56729a997380f27767b63e924601118a234
Fenist <kostik06@gmail.com>
2021-04-12 22:06:16 +0300

    Логирование регистрации, входа/выхода пользователей

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using WebStore.Domain.Entities.Identity;
 using WebStore.ViewModels;
+using Microsoft.Extensions.Logging;
+using System.Linq;
 
 namespace WebStore.Controllers
 {
@@ -10,11 +12,16 @@ namespace WebStore.Controllers
     {
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
+        private readonly ILogger<AccountController> _logger;
 
-        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        public AccountController(
+            UserManager<User> userManager,
+            SignInManager<User> signInManager,
+            ILogger<AccountController> logger)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _logger = logger;
         }
 
         #region Register
@@ -30,16 +37,28 @@ namespace WebStore.Controllers
                 UserName = model.UserName
             };
 
+            _logger.LogInformation("Регистрация пользователя {0}", user.UserName);
+
             var registration_result = await _userManager.CreateAsync(user, model.Password);
 
             if (registration_result.Succeeded)
             {
+                _logger.LogInformation("Пользователь {0} успешно зарегистрирован", user.UserName);
+
                 await _userManager.AddToRoleAsync(user, Role._users);
+
+                _logger.LogInformation("Пользователь {0} наделен ролью {1}", user.UserName, Role._users);
 
                 await _signInManager.SignInAsync(user, false);
 
+                _logger.LogInformation("Пользователь {0} вошел в систему сразу после регистрации", user.UserName);
+
                 return RedirectToAction("Index", "Home");
             }
+
+            _logger.LogWarning("Ошибка при регистрации пользователя {0} : {1}",
+                user.UserName,
+                string.Join(",", registration_result.Errors.Select(e => e.Description)));
 
             foreach (var error in registration_result.Errors)
                 ModelState.AddModelError("", error.Description);
@@ -70,11 +89,15 @@ namespace WebStore.Controllers
 
             if (login_result.Succeeded)
             {
+                _logger.LogInformation("Пользователь {0} вошёл в систему", model.UserName);
                 //if (Url.IsLocalUrl(model.ReturnUrl))
                 //    return Redirect(model.ReturnUrl);
                 //return RedirectToAction("Index", "Home");
                 return LocalRedirect(model.ReturnUrl ?? "/");
             }
+
+
+            _logger.LogWarning("Ошибка при вводе имени пользователя {0}, либо пароля", model.UserName);
 
             ModelState.AddModelError("", "Неверное имя пользователя, или пароль!");
             return View(model);
@@ -83,7 +106,11 @@ namespace WebStore.Controllers
 
         public async Task<IActionResult> Logout()
         {
+            var user_name = User.Identity!.Name;
+
             await _signInManager.SignOutAsync();
+
+            _logger.LogInformation("Пользователь {0} вышел из системы", user_name);
             return RedirectToAction("Index", "Home");
         }
 
